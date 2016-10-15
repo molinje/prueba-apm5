565d3d736418555eb6bb0aff240f5a640d110986
alexospina7 <alexospina7@hotmail.com>
2016-10-14 21:08:29 -0500

    Lab de Responsive y JS

---
+++ Lab4/codigo.js
@@ -0,0 +1,17 @@
+var dias =["Lunes","Martes","Miercoles","Jueves","Viernes","Sábado","Domingo"];
+alert( "Esto es un Array" + "" + dias);
+
+var Numero1 = 10;
+var Numero2 = 30;
+var Numero3 = 0;
+
+var resultado = Numero1 + Numero2;
+
+function Suma_valores(Numero1, Numero2) {
+
+  var TotalSum = Numero1 + Numero2;
+  return TotalSum ;
+}
+ 
+//var TotalSuma = Suma_valores(Numero1, Numero2);
+alert( "El resultado de la funcion es " + " " + Suma_valores(Numero1, Numero2));
