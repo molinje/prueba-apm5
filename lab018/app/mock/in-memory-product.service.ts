ff7188246649c293daeb06af0f26c240d1561510
molinje <molinje@gmail.com>
2016-10-29 12:58:38 -0500

    adicionar users a mock

--- lab018/app/mock/in-memory-product.service.ts
+++ lab018/app/mock/in-memory-product.service.ts
@@ -29,7 +29,34 @@ export class InMemoryProductService implements InMemoryDbService {
             }
         ];
 
-        return {products};
+         let users = [
+            {
+                id: 1,
+                name: "Samsung galaxy 8",
+                description: "telefono que explota",
+                type: "smartphone",
+                price: 1500000,
+                quantity: 20
+            },
+            {
+                id: 2,
+                name: "Samsung galaxy 10",
+                description: "telefono que explota",
+                type: "smartphone",
+                price: 1500000,
+                quantity: 20
+            },
+            {
+                id: 3,
+                name: "Samsung galaxy 20",
+                description: "telefono que explota",
+                type: "smartphone",
+                price: 1500000,
+                quantity: 20
+            }
+        ];
+
+        return {products, users};
     }
 
 }
