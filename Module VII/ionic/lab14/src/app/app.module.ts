739bc5b5a7750c7b8ac0f58b52b5e421efd2f1fa
molinje <molinje@gmail.com>
2016-11-25 18:59:33 -0500

    actionSheet

--- Module VII/ionic/lab14/src/app/app.module.ts
+++ Module VII/ionic/lab14/src/app/app.module.ts
@@ -1,12 +1,23 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+﻿import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { Page1 } from '../pages/page1/page1';
+import { Page2 } from '../pages/page2/page2';
+import {ModalProfilePage} from "../pages/modal-profile/modal-profile";
+import { Page3 } from '../pages/page3/page3';
+import { Page4 } from '../pages/page4/page4';
+import { Page5 } from '../pages/page5/page5';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    Page1,
+    Page2,
+    ModalProfilePage,
+    Page3,
+    Page4,
+    Page5
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,8 +25,13 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    Page1,
+    Page2,
+    ModalProfilePage,
+    Page3,
+    Page4,
+    Page5
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: []
 })
 export class AppModule {}
